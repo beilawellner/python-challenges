e809a5a7a80a9492dad4f37d28091a3c90dcddd1
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2024-11-02 23:24:29 +0200

    Time: 1 ms (17.27%), Space: 16.5 MB (80.73%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.py
+++ 2580-circular-sentence/2580-circular-sentence.py
@@ -1,8 +1,6 @@
 class Solution:
-    def isCircularSentence(self, sentence: str) -> bool:
-        spaces = [index for index, char in enumerate(sentence) if char == ' ']
-        if spaces:
-            for space in spaces:
-                if sentence[space-1] != sentence[space+1]:
-                    return False
-        return sentence[-1] == sentence[0]
+  def isCircularSentence(self, sentence: str) -> bool:
+    for i, c in enumerate(sentence):
+      if c == ' ' and sentence[i - 1] != sentence[i + 1]:
+        return False
+    return sentence[0] == sentence[-1]
