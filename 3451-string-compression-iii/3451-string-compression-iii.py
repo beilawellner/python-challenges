40b137d9535d79a07ec5967670b33fe27d196daf
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2024-11-04 22:54:58 +0200

    Time: 223 ms (62.03%), Space: 18.8 MB (96.07%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.py
+++ 3451-string-compression-iii/3451-string-compression-iii.py
@@ -6,13 +6,15 @@ count = 1
 class Solution:
     def compressedString(self, word: str) -> str:
         comp = ""
-        while word:
-            c = word[0]
+        i = 0
+        while i < len(word):
+            c = word[i]
             count = 1
-            while count < len(word) and word[count] == c:
+            i += 1
+            while i < len(word) and word[i] == c:
                 if count == 9:
                     break
                 count += 1
+                i += 1
             comp += f"{count}{c}"
-            word = word[count:]
         return comp
