7729dc4f3f4115d83c3a89702356396517a76154
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2024-11-18 21:42:08 +0200

    Time: 4 ms (47.59%), Space: 16.6 MB (62.6%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.py
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.py
@@ -1,13 +1,44 @@
+'''
+code = [5,7,1,4], k = 3
+        s
+              e
+code = [5,7,1,4], k = -3
+        s
+              e
+res  = [0,0,0,0]
+'''
 class Solution:
+    # sliding window
     def decrypt(self, code: List[int], k: int) -> List[int]:
-        result = [0] * len(code)
+        n = len(code)
+        res = [0] * n
         if k == 0:
-            return result
-        for i in range(len(result)):
-            if k > 0:
-                for j in range(i + 1, i + k + 1):
-                    result[i] += code[j % len(code)]
-            else:
-                for j in range(i + k, i):
-                    result[i] += code[(j + len(code)) % len(code)]
-        return result
+            return res
+        if k > 0:
+            start, end = 1, k
+        else:
+            start, end = n + k, n - 1
+        win_sum = sum(code[i % n] for i in range(start, end + 1))
+        for i in range(n):
+            res[i] = win_sum
+            win_sum -= code[start % n]
+            start += 1
+            end += 1
+            win_sum += code[end % n]
+        return res
+            
+
+
+    # # # # # # # # # # # # Brute Force
+    # def decrypt(self, code: List[int], k: int) -> List[int]:
+    #     result = [0] * len(code)
+    #     if k == 0:
+    #         return result
+    #     for i in range(len(result)):
+    #         if k > 0:
+    #             for j in range(i + 1, i + k + 1):
+    #                 result[i] += code[j % len(code)]
+    #         else:
+    #             for j in range(i + k, i):
+    #                 result[i] += code[(j + len(code)) % len(code)]
+    #     return result
