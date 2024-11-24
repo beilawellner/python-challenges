16f0f29c2fe6d4af84e05f233e46faf4eb3be63f
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2024-11-24 23:46:10 +0200

    Time: 0 ms (100%), Space: 16.7 MB (36.68%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -10,18 +10,24 @@ class Solution:
         p1 = 0
         num = 0
 
-        for i, c in enumerate(abbr):
-            if c.isdigit():
-                if num == 0 and c == '0':
+        i = 0
+        while i < len(abbr):
+            if abbr[i].isdigit():
+                if abbr[i] == '0':
                     return False
-                num = (num * 10) + int(c)
-            elif c.isalpha():
-                if num > 0:
-                    p1 += num
-                    num = 0
-                if p1 >= len(word) or c != word[p1]:
+
+                start = i
+                while i < len(abbr) and abbr[i].isdigit():
+                    i += 1
+                num = int(abbr[start:i])
+                p1 += num
+                num = 0
+                # num = (num * 10) + int(c)
+            elif abbr[i].isalpha():
+                if p1 >= len(word) or abbr[i] != word[p1]:
                     return False
                 p1 += 1
+                i += 1
         if num > 0:
             p1 += num
 
