df5a687b8061efac0f9ff92d5d8ac5568f39e522
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2024-11-24 12:05:35 +0200

    Time: 90 ms (53.85%), Space: 25.1 MB (41.3%) - LeetHub

--- 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
@@ -1,14 +1,12 @@
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        rows, cols = len(matrix), len(matrix[0])
         min_val = abs(matrix[0][0])
         count_neg = 0
         summation = 0
-        for r in range(rows):
-            for c in range(cols):
-                if matrix[r][c] < 0:
+        for row in matrix:
+            for n in row:
+                if n < 0:
                     count_neg += 1
-                abs_val = abs(matrix[r][c])
-                min_val = min(min_val, abs_val)
-                summation += abs_val
+                min_val = min(min_val, abs(n))
+                summation += abs(n)
         return summation if count_neg % 2 == 0 else (summation - 2 * min_val)
