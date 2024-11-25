726912d8b42488bf132abdb7a89d8a9f71a3ad4f
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2024-11-25 12:18:00 +0200

    Time: 3 ms (97.98%), Space: 17 MB (5.52%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -4,16 +4,20 @@ class Solution:
 
         def dfs(i, cur_lst, total):
             if total == target:
-                res.append(cur_lst.copy())
+                res.append(cur_lst[:])
                 return
 
-            if i >= len(candidates) or total > target:
-                return
+            for j in range(i, len(candidates)):
+                if total + candidates[j] <= target:
+                    dfs(j, cur_lst[:] + [candidates[j]], total + candidates[j])
 
-            cur_lst.append(candidates[i])
-            dfs(i, cur_lst, total + candidates[i])
-            cur_lst.pop()
-            dfs(i + 1, cur_lst, total)
+            # if i >= len(candidates) or total > target:
+            #     return
+
+            # cur_lst.append(candidates[i])
+            # dfs(i, cur_lst, total + candidates[i])
+            # cur_lst.pop()
+            # dfs(i + 1, cur_lst, total)
 
         dfs(0, [], 0)
         return res
