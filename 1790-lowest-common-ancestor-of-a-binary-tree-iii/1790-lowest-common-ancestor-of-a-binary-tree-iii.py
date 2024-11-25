a565f2904b98b83e1da5e8103b7267def27dc059
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2024-11-25 09:10:45 +0200

    Time: 56 ms (17.11%), Space: 20.1 MB (34.99%) - LeetHub

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -7,27 +7,16 @@ class Node:
         self.right = None
         self.parent = None
 """
-'''
-root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
-stack_p = [5, 3]
-stack_q = [1, 3]
-'''
-
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        stack_p, stack_q = [p], [q]
-        common_parent = None
+        seen = set()
 
-        while p.parent or q.parent:
-            p, q = stack_p[-1], stack_q[-1]
-            if p.parent:
-                stack_p.append(p.parent)
-            if q.parent:
-                stack_q.append(q.parent)
+        while p:
+            seen.add(p)
+            p = p.parent
 
-        while stack_p and stack_q:
-            p, q = stack_p.pop(), stack_q.pop()
-            if p == q:
-                common_parent = p
-
-        return common_parent
+        while q:
+            if q in seen:
+                return q
+            seen.add(q)
+            q = q.parent
