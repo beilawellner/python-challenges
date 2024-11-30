003517d9a695aa6db0384112fe5a4a6449bb8840
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2024-11-30 21:30:24 +0200

    Time: 86 ms (31.48%), Space: 17.2 MB (6.7%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.py
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.py
@@ -1,17 +1,24 @@
 '''
 nums = [2,5,7,8,9,2,3,4,3,1], k = 3
+        s
+            i
+last_start = 0
+subsets_starts = {0,1,2}
 '''
 class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
-        n = len(nums)
-
-        def is_increacing(start):
-            for i in range(start + 1, start + k):
-                if start + k - 1 >= len(nums) or nums[i] <= nums[i - 1]:
-                    return False
+        if k == 1:
             return True
 
-        for i in range(n - k + 1):
-            if is_increacing(i) and is_increacing(i + k):
-                return True
+        subsets_start = set()
+        start = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                start = i
+            elif i - start + 1 == k:
+                if start - k in subsets_start:
+                    return True
+                subsets_start.add(start)
+                start += 1
         return False
