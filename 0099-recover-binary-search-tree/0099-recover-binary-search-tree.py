6ff08eb5878c9a1177a89cbd6eefa22fb2c5b822
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2024-12-01 21:50:07 +0200

    Time: 1 ms (73.27%), Space: 17.4 MB (7.1%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.py
@@ -4,41 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-'''
-root = [3,1,4,null,null,2]
-inorder = [1,3,2,4]
-y, x = 2, 3
-'''
 class Solution:
-    def recoverTree(self, root: TreeNode) -> None:
-        def inorder(r: TreeNode) -> List[int]:
-            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        stack = []
+        x = y = prev = None
 
-        def find_two_swapped(nums: List[int]) -> (int, int):
-            n = len(nums)
-            x = y = None
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if prev and root.val < prev.val:
+                y = root
+                if not x:
+                    x = prev
+                else:
+                    break
+            prev = root
+            root = root.right
 
-            for i in range(n - 1):
-                if nums[i + 1] < nums[i]:
-                    y = nums[i + 1]
-                    # The first swap occurrence
-                    if x is None:
-                        x = nums[i]
-                    # The second swap occurrence
-                    else:
-                        break
-            return x, y
-
-        def recover(r: TreeNode, count: int) -> None:
-            if r:
-                if r.val == x or r.val == y:
-                    r.val = y if r.val == x else x
-                    count -= 1
-                    if count == 0:
-                        return
-                recover(r.left, count)
-                recover(r.right, count)
-
-        nums = inorder(root)
-        x, y = find_two_swapped(nums)
-        recover(root, 2)
+        x.val, y.val = y.val, x.val
+        
