3c3007ef239332824f1f93760e2f79d3e327415a
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2024-12-03 09:27:10 +0200

    Time: 12 ms (33.62%), Space: 20.8 MB (10.2%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -1,13 +1,17 @@
 class MovingAverage:
 
     def __init__(self, size: int):
-        self.array = []
+        self.queue = deque()
         self.size = size
+        self.total = 0
 
     def next(self, val: int) -> float:
-        self.array.append(val)
+        if len(self.queue) == self.size:
+            self.total -= self.queue.popleft()
+        self.total += val
+        self.queue.append(val)
         
-        return sum(self.array[-self.size:]) / min(self.size, len(self.array))
+        return self.total / min(self.size, len(self.queue))
         
 
 
